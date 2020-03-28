5f57b4cda7c9d979cbc1e3ddf93451fd17a3a457
lidan <1214434643@qq.com>
2020-03-28 21:46:27 +0800

    render性能处理(解读react生命周期)

--- src/components/TodoList/TodoItem/index.js
+++ src/components/TodoList/TodoItem/index.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
 const noop = () => {};
 export default class index extends Component {
+  // //老版本的坑：constructor里面通过props来初始化了一个state，在props修改之后，这个state不会再次更新
+  // constructor(props) {
+  //   super();
+  //   this.state = {
+  //     componentText: props.isComponented ? "已完成" : "未完成"
+  //   };
+  // }
+  // //那么需要借助于UNSAFE_componentWillReceiveProps来做一次修正
+  // UNSAFE_componentWillReceiveProps(nextProps) {
+  //   this.setState({
+  //     componentText: nextProps.isComponented ? "已完成" : "未完成"
+  //   });
+  // }
+
+  constructor() {
+    super();
+    this.state = {
+      componentText: ""
+    };
+  }
+  //静态方法没有this，在对象创建之前static就会执行
+  static getDerivedStateFromProps(props) {
+    //不管是更新还是初始化中都会执行
+    return {
+      componentText: props.isComponented ? "已完成" : "未完成"
+    };
+  }
   handelChange = () => {
     // this.props.onCompeletedChange &&
     //   this.props.onCompeletedChange(this.props.id);
@@ -8,7 +35,13 @@ export default class index extends Component {
     const { onCompeletedChange = noop, id } = this.props;
     onCompeletedChange(id);
   };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.isComponented !== this.props.isComponented;
+  }
+
   render() {
+    console.log(`${this.props.title}`);
     const { id, title, isComponented } = this.props;
     return (
       <li key={id} id={id}>
@@ -18,7 +51,8 @@ export default class index extends Component {
           onChange={this.handelChange}
         />
         <span>
-          {title} {isComponented ? "已完成" : "未完成"}
+          {/* {title} {isComponented ? "已完成" : "未完成"} */}
+          {title} {this.state.componentText}
         </span>
       </li>
     );

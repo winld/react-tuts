f2a25cf237d10b6d0b97ed84a1913756e46e0aa4
lidan <1214434643@qq.com>
2020-03-29 22:11:02 +0800

    react hooks学习

--- src/index.js
+++ src/index.js
@@ -1,50 +1,39 @@
-import React from "react";
+//React Hooks是react16.8（16.7alpha）更新的一项特性，它可以让你在不便携class的情况下使用state以及其他的React特性
+
+//两个常见的api，就是useState和useEffect，需要先引入
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
+//useState是一个方法，这个方法的参数就是默认值，结果是一个数组，数组的元素第一个就是state，第二个就相当于setState
+//解构出来的数组中的两个值
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  //useEffect的参数就是一个回调，不管是组件挂载还是更新都会触发这个回调方法，
+  //类似于componentDidMount和componentDidUpdate的结合
+  useEffect(() => {
+    document.title = `当前count是：${count}`;
+  });
+  return (
+    <div>
+      <h5>当前count是：{count}</h5>
+      {/* 这里的setCount就是所生成的方法（第二个），注意的setState不一样的地方在于参数，这里的参数就是一个心智即可 */}
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      {/* 这里就是setState创建的值（第一个） */}
+      <span>{count}</span>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
 
-//定义组件的 定义组件的第二种方式，使用类继承React.Component
-// const app = {
-//   tag: "div",
-//   attr: {
-//     className: "app",
-//     id: "appRoot"
-//   },
-//   children: [
-//     {
-//       tag: "h1",
-//       attr: {
-//         className: title
-//       },
-//       children: ["jsx的原理"]
-//     }
-//   ]
-// };
-//所以rect在真正的渲染的时候会把上面的代码变异为下面的样子来圆形，下面的代码就是合法的js代码
-class App extends React.Component {
-  render() {
-    return React.createElement(
-      "div",
-      { className: "app", id: "appRoot" },
-      React.createElement("h1", { className: "title" }, "sdgsgs")
-    );
-    // <div className='app' id='appRoot'>
-    //   <h1 className='title'>{this.props.titile}</h1>
-    // </div>
-  }
-}
-//类组件渲染的原理
-// const app = new App({
-//   title: "创建组件的第二种方式：通过类创建"
-// }).render();
-
-//render是reactdom提供的一个方法，这个方法通常只会用一次
-render(
-  <App titile='创建组件的第二种方式：通过类创建' />,
-  document.querySelector("#root")
-);
-
-// 在react 16以前，使用这种方式来创建的一个类组件
-// React.creactClass({
-//    render(){
-//    Renturn <h1>dfsd</h1>
-// }
-// })
+render(<Counter />, document.querySelector("#root"));

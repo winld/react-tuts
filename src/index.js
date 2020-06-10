bca8b2cab2d32c3efd93634139cf511a9e7380d5
lidan <1214434643@qq.com>
2020-06-10 18:18:14 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./App";
+import React from 'react';
+import { render } from 'react-dom';
 
-render(<App />, document.querySelector("#root"));
+render(<div />, document.querySelector('#root'));
